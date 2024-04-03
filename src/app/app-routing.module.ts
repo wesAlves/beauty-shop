e1c0e21d81524396762a36c283dbb7a15964217d
Wes <wal2.designer@gmail.com>
2024-04-03 12:15:10 +0100

    wip: adding routes and url params

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {EventsComponent} from "./events/events.component";
+import {DatailComponent} from "./events/datail/datail.component";
 
 const routes: Routes = [
-  {path: "", redirectTo: '/dashboard', pathMatch: "full" },
-  {path: "dashboard", component: DashboardComponent },
+  {path: "", redirectTo: '/dashboard', pathMatch: "full"},
+  {
+    path: "dashboard", component: DashboardComponent, children: [
+      {path: "", component: EventsComponent},
+      {path: ":id", component: DatailComponent}
+    ]
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,12 +4,12 @@ import {Component, Input, OnInit} from '@angular/core';
 import {appointments} from '../../api/api';
 
 
-interface IAppointment {
-  dateTime: Date,
-  costumer: string,
-  service: string,
-  serviceDescription: string
-}
+// interface IAppointment {
+//   dateTime: Date,
+//   costumer: string,
+//   service: string,
+//   serviceDescription: string
+// }
 
 @Component({
   selector: 'app-dashboard',
@@ -19,32 +19,32 @@ interface IAppointment {
 export class DashboardComponent implements OnInit {
 
   selectedDay: Date = new Date();
-  appointments: IAppointment[] = []
+  // appointments: IAppointment[] = []
 
   constructor() {
   }
 
   ngOnInit() {
-    this.appointmentsByDate(this.selectedDay);
+    // this.appointmentsByDate(this.selectedDay);
   }
 
-  private appointmentsByDate(day: Date) {
-    this.appointments = [];
-
-    for (let appointment of appointments) {
-      const newAppointment = {...appointment, dateTime: new Date(appointment.dateTime)}
-
-      if (newAppointment.dateTime.toDateString() === day.toDateString()) {
-        this.appointments.push(newAppointment)
-      }
-
-    }
-  }
+  // private appointmentsByDate(day: Date) {
+  //   this.appointments = [];
+  //
+  //   for (let appointment of appointments) {
+  //     const newAppointment = {...appointment, dateTime: new Date(appointment.dateTime)}
+  //
+  //     if (newAppointment.dateTime.toDateString() === day.toDateString()) {
+  //       this.appointments.push(newAppointment)
+  //     }
+  //
+  //   }
+  // }
 
   serverElement(day: Date) {
     this.selectedDay = day
 
-    this.appointmentsByDate(this.selectedDay);
+    // this.appointmentsByDate(this.selectedDay);
 
   }
 
