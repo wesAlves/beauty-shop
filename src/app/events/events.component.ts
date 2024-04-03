8ea80bfe87c234b31075c9076184fd901b7ef767
Wes <wal2.designer@gmail.com>
2024-04-03 17:02:42 +0100

    Adding events component

---
+++ src/app/events/events.component.ts
@@ -0,0 +1,53 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {appointments} from "../../api/api";
+import {ActivatedRoute, Params} from "@angular/router";
+
+
+interface IAppointment {
+  dateTime: Date,
+  costumer: string,
+  service: string,
+  serviceDescription: string
+}
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrl: './events.component.scss'
+})
+export class EventsComponent implements OnInit {
+  appointments: IAppointment[] = []
+  month: string = "";
+  year: string = "";
+  date: string = "";
+
+
+  constructor(private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.activatedRoute.queryParams
+      .subscribe(
+        (qParams: Params) => {
+          const {year, month, date} = qParams
+
+          this.appointmentsByDate(new Date(year, month, date))
+        }
+  )
+
+  }
+
+  private appointmentsByDate(day: Date) {
+    this.appointments = [];
+
+    for (let appointment of appointments) {
+      const newAppointment = {...appointment, dateTime: new Date(appointment.dateTime)}
+
+      if (newAppointment.dateTime.toDateString() === day.toDateString()) {
+        this.appointments.push(newAppointment)
+      }
+
+    }
+  }
+
+}
